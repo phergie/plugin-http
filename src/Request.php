db7a734bab5b6c60f90aa59b660dde48ef0b37bd
Cees-Jan <ceesjank@gmail.com>
2014-05-06 22:05:03 +0200

    Request class skeleton #1

---
+++ src/Request.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of PhergieHttp.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace WyriHaximus\Phergie\Plugin\Http;
+
+/**
+ * Plugin for Provide HTTP functionality to other plugins.
+ *
+ * @category Phergie
+ * @package WyriHaximus\Phergie\Plugin\Http
+ */
+class Request
+{
+    /**
+     * Accepts plugin configuration.
+     *
+     * Supported keys:
+     *
+     *
+     *
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+
+        $this->setDefaults();
+    }
+
+    public function getConfig() {
+        return $this->config;
+    }
+
+    protected function setDefaults() {
+
+    }
+}
