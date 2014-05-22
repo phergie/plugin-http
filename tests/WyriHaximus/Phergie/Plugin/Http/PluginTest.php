fc460fe096ddbdf25e51e1e3ef6f4058f2ac8069
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-05-22 20:56:42 +0200

    Bare test file

---
+++ tests/WyriHaximus/Phergie/Plugin/Http/PluginTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of PhergieHttp.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WyriHaximus\Phergie\Plugin\Http;
+
+/**
+ * Tests for the Plugin class.
+ *
+ * @category Phergie
+ * @package WyriHaximus\Phergie\Plugin\Http
+ */
+class PluginTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetSubscribedEvents()
+    {
+        $plugin = new Plugin();
+        $subscribedEvents = $plugin->getSubscribedEvents();
+        $this->assertInternalType('array', $subscribedEvents);
+        $this->assertSame(array(
+            'http.request' => 'makeHttpRequest',
+        ), $subscribedEvents);
+    }
+}
